29c9b24712165a64d2b690ee543db191306f477d
kevinthomas <kthomas1208@gmail.com>
2016-07-02 02:15:47 -0400

    Added CollapsingToolBarLayout again

--- src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -42,8 +42,6 @@ public class ArticleListActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
 
-        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
-
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
